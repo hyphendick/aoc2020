66dcd7dbc835430338dcbd9480e3b7d577555dd2
hyphendick <hyphendick@users.noreply.github.com>
2021-12-01 19:55:33 -0500

    day 1 part2

--- 2021/1.js
+++ 2021/1.js
@@ -2,3 +2,9 @@
 $$('pre')[0].innerText.split('\n')
 var a = $$('pre')[0].innerText.split('\n')
 a.map((x,i) => {return a[i+1]-a[i]}).filter(x=>x >0).length
+
+
+$$('pre')[0].innerText.split('\n')
+var a = $$('pre')[0].innerText.split('\n')
+a = a.map((x,i) => {return parseInt(a[i])+parseInt(a[i+1])+parseInt(a[i+2])})
+a.map((x,i) => {return a[i+1]-a[i]}).filter(x=>x >0).length
