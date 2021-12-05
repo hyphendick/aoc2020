9f27e63b349028b03f06ad3859b97d0f4271ba5a
hyphendick <hyphendick@users.noreply.github.com>
2021-12-04 23:45:28 -0500

    3.2

--- 2021/3.js
+++ 2021/3.js
@@ -5,3 +5,29 @@ a.reduce((prev, cur) => {
 var nxt = cur.split('').map(x=>parseInt(x)).map((x,i) =>  prev[i]+x)
 return nxt
 }, Array(a[0].length).fill(0)).map(z => (z> a.length/2)*1)
+
+
+$$('pre')[0].innerText.split('\n')
+var a = $$('pre')[0].innerText.split('\n')
+a.pop()
+var inp = a
+for(var i = 0, e = a[0].length; i < e; i++)
+{
+  var res = calc(inp, i)
+  if (res.length ===1) {
+    console.log(res);
+    break;
+  }
+  inp = res
+}
+function calc(_inp, bit, neg){
+  var ones =[], zeroes =[]
+  var out = _inp.reduce((prev, cur) => {
+    cur[bit] === '1' ? ones.push(cur) : zeroes.push(cur)
+  var nxt = cur.split('').map(x=>parseInt(x)).map((x,i) =>  prev[i]+x)
+  return nxt
+  }, Array(_inp[0].length).fill(0)).map(z => (z>= _inp.length/2)*1) // < for other one
+  if (out[bit] === 1) {
+    return ones
+  } else { return zeroes}
+}
